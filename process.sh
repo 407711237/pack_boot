02e268beb8985371fe24bf287ef66e67ed58215b
XiaoHua <63435884+407711237@users.noreply.github.com>
2025-03-26 21:31:17 +0800

    Update process.sh

--- process.sh
+++ process.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # 定义文件路径
-PAYLOAD="payload.bin"
+PAYLOAD="${PAYLOAD_PATH}"
+KERNEL="${KERNEL_PATH}"
 EXTRACTED_BOOT="extracted_boot.img"
-OUTPUT="new_boot.img"
+OUTPUT="boot.img.new"
 
 # 使用magiskboot提取boot.img
 magiskboot extract "$PAYLOAD" boot "$EXTRACTED_BOOT"
@@ -13,7 +14,7 @@ magiskboot unpack "$EXTRACTED_BOOT"
 
 # 删除旧kernel并替换
 rm ./kernel
-mv kernel ./
+mv "$KERNEL" ./
 
 # 重新打包
 magiskboot repack boot.img "$OUTPUT"
