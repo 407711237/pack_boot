e60154949d08ceb4871072ddaa744996368093fc
XiaoHua <63435884+407711237@users.noreply.github.com>
2025-03-28 00:38:06 +0800

    更新 process.sh

--- process.sh
+++ process.sh
@@ -4,13 +4,13 @@
 BOOT="${BOOT_PATH}"
 KERNEL="${KERNEL_PATH}"
 
-cp "$BOOT"./boot.img
+cp "$BOOT" ./boot.img
 
 # 解包提取的boot.img
-magiskboot unpack./boot.img
+magiskboot unpack ./boot.img
 
 # 删除旧kernel并替换
 cp -f "$KERNEL" ./kernel
 
 # 重新打包
-magiskboot repack./boot.img
+magiskboot repack ./boot.img
